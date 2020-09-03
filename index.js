44ac3febb4957f15a0366650adcc13eb4d09dc32
Leonard Kriese <leonard.kriese@turbinekreuzberg.com>
2020-09-04 00:58:15 +0100

    rust benchmark

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "benchmark";
 
-wasm.greet();
+//wasm.greet();
+
+var start = new Date()
+
+wasm.iterate_array(1000, "Lorem Ipsum");
+var end = new Date() - start
+console.info('Execution time: %dms', end)
+
+var start = new Date()
+
+
+let my_string = ""
+for (let i = 0; i < 100000000; i++) {
+    my_string += "a";
+}
+
+var end = new Date() - start
+
+
+console.info('Execution time: %dms', end)
